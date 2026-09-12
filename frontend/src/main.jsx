8324f22e782c724864ce4ff3b150a83212fdd05b
studdybuddyevolution-wq <studdybuddyevolution@gmail.com>
2026-09-12 10:02:55 +0530

    Pin Apollo chat composer and keep sidebar visible

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './AppPhase5Clean'
 import './styles.css'
 import './phase5-layout-fix.css'
+import './chat-layout-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
