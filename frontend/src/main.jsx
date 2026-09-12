cd42327cd0905ea43c73ecc0c57235b8045cf306
studdybuddyevolution-wq <studdybuddyevolution@gmail.com>
2026-09-12 13:18:56 +0530

    Switch Apollo frontend to Phase 6 web-enabled shell

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppPhase5Clean'
+import App from './AppPhase6'
 import './styles.css'
 import './phase5-layout-fix.css'
 import './chat-layout-fix.css'
+import './phase6-web.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
