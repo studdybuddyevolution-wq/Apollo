00a5339e3d576ce0992dc2cd2331a80eaa24f352
studdybuddyevolution-wq <studdybuddyevolution@gmail.com>
2026-09-17 17:13:39 +0530

    feat: mount Phase 2 source importer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppPhase6'
+import Phase2ImportPanel from './Phase2ImportPanel'
 import './styles.css'
 import './phase5-layout-fix.css'
 import './chat-layout-fix.css'
@@ -9,5 +10,6 @@ import './phase6-web.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Phase2ImportPanel />
   </React.StrictMode>,
 )
